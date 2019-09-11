98950778a5854281409a3dd2d16e21920f7431be
Jesse <jessehaenen@gmail.com>
2019-09-11 16:27:05 +0200

    CookieCutter repo structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Demand Forecasting challenge on Kaggle',
+    author='Xomnia Class 15',
+    license='',
+)
